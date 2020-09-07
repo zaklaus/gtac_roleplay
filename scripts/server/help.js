f2bd794c3c65761d9cc1e5300bd3d9440497fe4a
VortrexFTW <adrianbram@gmail.com>
2020-09-07 11:34:26 -0500

    Add tips

--- scripts/server/help.js
+++ scripts/server/help.js
@@ -96,3 +96,67 @@ function showAccountHelpMessage(client) {
 }
 
 // ----------------------------------------------------------------------------
+
+function showEnteredDriverSeatHelpTip(client) {
+    if(getClientData(client).accountData.shownTips & !shownTipsFlags.enteredDriverSeat) {
+        //messageClientInfo(client, `Press ${serverConfig.keybindText.vehicleEngineKey} for engine, ${serverConfig.keybindText.vehicleLightsKey} for lights, and ${serverConfig.keybindText.vehicleLockKey} for door locks`);
+        getClientData(client).accountData.shownTips = getClientData(client).accountData.shownTips | shownTipsFlags.enteredDriverSeat;
+    }  
+}
+
+// ----------------------------------------------------------------------------
+
+function showApproachJobWhileUnemployedTip(client) {
+    if(getClientData(client).accountData.shownTips & !shownTipsFlags.approachJobWhileUnemployed) {
+        //messageClientInfo(client, `Approach the icon and press ${serverConfig.keybindText.actionKey} to see info about this job.`);
+        getClientData(client).accountData.shownTips = getClientData(client).accountData.shownTips | shownTipsFlags.approachJobWhileUnemployed;
+    }  
+}
+
+// ----------------------------------------------------------------------------
+
+function showTakeNearbyJobTip(client) {
+    if(getClientData(client).accountData.shownTips & !shownTipsFlags.takeJobWhileUnemployed) {
+        //messageClientInfo(client, `Use /takejob to take this job.`);
+        getClientData(client).accountData.shownTips = getClientData(client).accountData.shownTips | shownTipsFlags.takeJobWhileUnemployed;
+    }  
+}
+
+// ----------------------------------------------------------------------------
+
+function showApproachCurrentJobTip(client) {
+    if(getClientData(client).accountData.shownTips & !shownTipsFlags.approachCurrentJob) {
+        //messageClientInfo(client, `Press ${serverConfig.keybindText.actionKey} to start working, or hold ${serverConfig.keybindText.actionKey} to quit your job.`);
+        //messageClientInfo(client, `Use /startwork to start working, or /quitjob to quit your job.`);
+        getClientData(client).accountData.shownTips = getClientData(client).accountData.shownTips | shownTipsFlags.approachCurrentJob;
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+function showStartedWorkingTip(client) {
+    if(getClientData(client).accountData.shownTips & !shownTipsFlags.startedWorking) {
+        messageClientInfo(client, `Press ${serverConfig.keybindText.actionKey} to change uniform, or hold ${serverConfig.keybindText.actionKey} to stop working.`);
+        getClientData(client).accountData.shownTips = getClientData(client).accountData.shownTips | shownTipsFlags.startedWorking;
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+function showApproachOwnedVehicleTip(client) {
+    if(getClientData(client).accountData.shownTips & !shownTipsFlags.approachOwnedVehicle) {
+        messageClientInfo(client, `Press ${serverConfig.keybindText.actionKey} to see vehicle info, or press ${serverConfig.keybindText.vehicleLockKey} to lock and unlock the vehicle.`);
+        getClientData(client).accountData.shownTips = getClientData(client).accountData.shownTips | shownTipsFlags.approachOwnedVehicle;
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+function showApproachAnyVehicleTip(client) {
+    if(getClientData(client).accountData.shownTips & !shownTipsFlags.approachAnyVehicle) {
+        messageClientInfo(client, `Press ${serverConfig.keybindText.actionKey} to see vehicle info.`);
+        getClientData(client).accountData.shownTips = getClientData(client).accountData.shownTips | shownTipsFlags.approachAnyVehicle;
+    }
+}
+
+// ----------------------------------------------------------------------------

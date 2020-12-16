3aa7e0ee518330e5594bb21e93321f0c95327225
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2020-12-16 07:56:54 -0600

    Remove old event stuff

--- scripts/server/event.js
+++ scripts/server/event.js
@@ -44,70 +44,6 @@ addEventHandler("OnPedSpawn", function(event, ped) {
 
 // ---------------------------------------------------------------------------
 
-addEventHandler("OnPedWasted", function(event, wastedPed, killerPed, weaponId, pedPiece) {
-    if(!wastedPed) {
-        return false;
-    }
-
-    //if(wastedPed.isType(ELEMENT_PLAYER)) {
-    //    processPlayerDeath(wastedPed);
-    //}
-});
-
-// ---------------------------------------------------------------------------
-
-bindEventHandler("OnResourceStop", thisResource, function(event, resource) {
-    //console.log("[Asshat.Event]: Resource stopping. Saving all data to database ...");
-    //saveAllClientsToDatabase();
-    //console.log("[Asshat.Event]: All data saved to database successfully!");
-});
-
-// ---------------------------------------------------------------------------
-
-bindEventHandler("OnResourceStart", thisResource, function(event, resource) {
-    //initAllClients();    
-});
-
-// ---------------------------------------------------------------------------
-
-addEventHandler("onPedEnterVehicle", function(event, ped, vehicle, seat) {
-    //ped.setData("ag.vehSeat", seat, false);
-
-    //if(!vehicle || vehicle.owner != -1) {
-    //    return false;
-    //}
-
-    //let vehicleData = getVehicleData(vehicle);
-    //if(vehicleData == null) {
-    //    return false;
-    //}
-
-    //if(ped.isType(ELEMENT_PLAYER)) {
-    //    let client = getClientFromPlayerElement(ped);
-    //    clientEnteredVehicle(client);
-    //}
-});
-
-// ---------------------------------------------------------------------------
-
-addEventHandler("onProcess", function(event, deltaTime) {
-    //if(!isGTAIV()) {
-    //    let clients = getClients();
-    //    for(let i in clients) {
-    //        if(!clients[i].console) {
-    //            if(clients[i].getData("ag.isPlaying")) {
-    //                if(clients[i].player.health <= 0) {
-    //                    console.log(clients[i].name + " died");
-    //                    processPlayerDeath(clients[i]);
-    //                }
-    //            }
-    //        }
-    //    }        
-    //}
-});
-
-// ---------------------------------------------------------------------------
-
 addEventHandler("onPlayerChat", function(event, client, messageText) {
     event.preventDefault();
     if(!getClientData(client).loggedIn) {
@@ -132,28 +68,50 @@ addEventHandler("OnPedExitVehicle", function(event, ped, vehicle) {
 
 // ---------------------------------------------------------------------------
 
-function clientEnteredVehicle(client, vehicleData) {
+function playerEnteredVehicle(client, vehicle) {
+    if(!vehicle || vehicle == null) {
+        return false;
+    }
+
+    if(!getVehicleData(vehicle)) {
+        return false;
+    }
+
+    if(!getVehicleData(vehicle).engine) {
+        messageClientTip(client, `This ${getVehicleName(vehicle)}'s engine is off. Use /engine to start it`);
+        if(doesPlayerHaveKeyBindForCommand(client, "engine")) {
+            messageClientTip(client, `You can also press ${getPlayerKeyBindForCommand(client, "engine")} to start and stop the engine.`);
+        }
+        triggerNetworkEvent("ag.control", client, false, false);
+    }
+
     let currentSubAccount = getClientCurrentSubAccount(client);
 
-    if(currentSubAccount.isWorking) {
-        if(vehicleData.ownerType == AG_VEHOWNER_JOB) {
-            if(vehicleData.ownerId == getJobType(currentSubAccount.job)) {
-                if(seat == 0) {
-                    getClientCurrentSubAccount(client).lastJobVehicle = vehicleData.syncId;
-                }
+    if(isPlayerWorking(client)) {
+        if(getVehicleData(vehicle).ownerType == AG_VEHOWNER_JOB) {
+            if(getVehicleData(vehicle).ownerId == getClientCurrentSubAccount(client).job) {
+                //if(seat == 0) {
+                    getClientCurrentSubAccount(client).lastJobVehicle = vehicle;
+                //}
             }
         }
     }
 
-    if(vehicleData.buyPrice > 0) {
-        messageClientInfo(client, `This vehicle is for sale! Cost $${vehicleData.buyPrice}`);
-        messageClientInfo(client, `Use /vehbuy if you want to buy this vehicle.`);
-    } else {
-        if(vehicleData.rentPrice > 0) {
-            messageClientInfo(client, `This vehicle is for rent! Cost: $${vehicleData.rentPrice} per minute`);
-            messageClientInfo(client, `Use /vehrent if you want to rent this vehicle.`);
+    if(getPlayerVehicleSeat(client) == AG_VEHSEAT_DRIVER) {
+        if(getVehicleData(vehicle).buyPrice > 0) {
+            messageClientAlert(client, `This ${getVehicleName(vehicle)} is for sale! Cost $${getVehicleData(vehicle).buyPrice}`);
+            messageClientTip(client, `Use /vehbuy if you want to buy it.`);
+        } else if(getVehicleData(vehicle).rentPrice > 0) {
+            messageClientAlert(client, `This ${getVehicleName(vehicle)} is for rent! Cost: $${getVehicleData(vehicle).rentPrice} per minute`);
+            messageClientTip(client, `Use /vehrent if you want to rent it.`);
         }
     }
+}
+
+// ---------------------------------------------------------------------------
+
+function playerExitedVehicle(client, vehicle) {
+
 }
 
 // ---------------------------------------------------------------------------

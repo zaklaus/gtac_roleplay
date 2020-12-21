cf9043defb7846205080342e621d7bbdb06d6663
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2020-12-21 09:24:39 -0600

    Added anticheat config & remove colour from config

--- scripts/server/config.js
+++ scripts/server/config.js
@@ -19,41 +19,11 @@ let serverConfig = {
 	groundSnow: 0,
 	showLogo: true,	
 	guiColour: [200, 200, 200],
-	colour: {
-		byType: {
-			talkMessage: toColour(200, 200, 200),
-			shoutMessage: toColour(255, 255, 200),
-			whisperMessage: toColour(130, 130, 130),
-			doActionMessage: toColour(153, 50, 204, 255),
-			meActionMessage: toColour(153, 50, 204, 255),
-			errorMessage: toColour(237, 67, 55, 255),
-			syntaxMessage: toColour(200, 200, 200, 255),
-			normalMessage: toColour(255, 255, 255, 255),
-			alertMessage: toColour(255, 255, 0, 255),
-			successMessage: toColour(0, 180, 0, 255),
-			clanChatMessage: toColour(0, 190, 0, 255),
-		},
-		byName: {
-			white: toColour(255, 255, 255, 255),
-			black: toColour(0, 0, 0, 255),
-			red: toColour(255, 0, 0, 255),
-			yellow: toColour(255, 255, 0, 255),
-			royalBlue: toColour(0, 0, 255, 255),
-			teal: toColour(0, 255, 255, 255),
-			orange: toColour(255, 128, 0, 255),
-			lightGrey: toColour(200, 200, 200, 255),
-			mediumGrey: toColour(150, 150, 150, 255),
-			darkGrey: toColour(64, 64, 64, 255),
-			policeBlue: toColour(70, 130, 180, 255),
-			medicPink: toColour(219, 112, 147, 255),
-			firefighterRed: toColour(205, 92, 92, 255),
-			busDriverGreen: toColour(50, 205, 50, 255),
-			taxiDriverYellow: toColour(240, 230, 140, 255),
-			burntYellow: toColour(210, 210, 0, 255),
-			burntOrange: toColour(210, 120, 0, 255),
-			bankGreen: toColour(0, 150, 0, 255),
-			softGreen: toColour(144, 255, 96, 255),
-		}
+	antiCheat: {
+		enabled: true,
+		checkGameScripts: true,
+		gameScriptBlackList: [],
+		gameScriptWhiteList: [],
 	},
 	accountPasswordHash: "SHA512",
 	connectCameraPosition: false,
@@ -293,6 +263,12 @@ function loadServerConfig() {
 				getServerConfig().groundSnow = intToBool(dbAssoc["svr_start_snow_ground"]);
 				getServerConfig().useGUI = intToBool(dbAssoc["svr_gui"]);
 				getServerConfig().guiColour = [toInteger(dbAssoc["svr_gui_col1_r"]), toInteger(dbAssoc["svr_gui_col1_g"]), toInteger(dbAssoc["svr_gui_col1_b"])];
+
+				getServerConfig().antiCheat.enabled = intToBool(dbAssoc["svr_ac_enabled"]);
+				getServerConfig().antiCheat.checkGameScripts = intToBool(dbAssoc["svr_ac_check_scripts"]);
+
+				getServerConfig().antiCheat.gameScriptWhiteList = loadAntiCheatGameScriptWhiteListFromDatabase(serverId);
+				getServerConfig().antiCheat.gameScriptBlackList = loadAntiCheatGameScriptBlackListFromDatabase(serverId);
 
 				applyConfigToServer();
 

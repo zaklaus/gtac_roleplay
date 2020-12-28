8e24513d05d043ddaaa38555bd4c792db46a185d
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2020-12-27 22:26:33 -0600

    Add accents list

---
+++ scripts/server/accent.js
@@ -0,0 +1,42 @@
+// ===========================================================================
+// Asshat-Gaming Roleplay
+// https://github.com/VortrexFTW/gtac_asshat_rp
+// Copyright (c) 2020 Asshat-Gaming (https://asshatgaming.com)
+// ---------------------------------------------------------------------------
+// FILE: accent.js
+// DESC: Provides accent functions and usage
+// TYPE: Server (JavaScript)
+// ===========================================================================
+
+let accents = [
+    "English",
+	"French",
+	"Russian",
+	"Scottish",
+	"Irish",
+	"Spanish",
+	"Southern American",
+	"Italian",
+	"Australian",
+	"Jamaican",
+	"Israeli",
+	"Dutch",
+	"Brazilian",
+	"Portuguese",
+	"German",
+	"Canadian",
+	"Chinese",
+	"Japanese",
+	"Turkish",
+	"Korean",
+	"Estonian",
+	"Sicilian",
+];
+
+// ---------------------------------------------------------------------------
+
+function getPlayerAccentText(client) {
+    return getClientCurrentSubAccount(client).accent;
+}
+
+// ---------------------------------------------------------------------------

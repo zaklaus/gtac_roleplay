bde624cbe9d902ca92e678fab894af9fbc368504
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2020-12-29 00:16:55 -0600

    Add shared constants

---
+++ scripts/shared/const.js
@@ -0,0 +1,14 @@
+// ===========================================================================
+// Asshat-Gaming Roleplay
+// https://github.com/VortrexFTW/gtac_asshat_rp
+// Copyright (c) 2020 Asshat-Gaming (https://asshatgaming.com)
+// ---------------------------------------------------------------------------
+// FILE: const.js
+// DESC: Provides constants
+// TYPE: Shared (JavaScript)
+// ===========================================================================
+
+// Label Types
+const AG_LABEL_JOB = 1;
+const AG_LABEL_BUSINESS = 2;
+const AG_LABEL_HOUSE = 3;

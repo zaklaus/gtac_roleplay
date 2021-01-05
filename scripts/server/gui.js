24d7441f0d300be737393c83b631379122018c63
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-01-05 01:23:46 -0600

    Add serverside GUI utils

---
+++ scripts/server/gui.js
@@ -0,0 +1,41 @@
+// ===========================================================================
+// Asshat-Gaming Roleplay
+// https://github.com/VortrexFTW/gtac_asshat_rp
+// Copyright (c) 2020 Asshat-Gaming (https://asshatgaming.com)
+// ---------------------------------------------------------------------------
+// FILE: gui.js
+// DESC: Provides GUI functions and usage
+// TYPE: Server (JavaScript)
+// ===========================================================================
+
+// UNFINISHED. NOT READY TO USE
+
+function showPlayerLoginGUI(client, errorMessage = "") {
+
+}
+
+// ---------------------------------------------------------------------------
+
+function showPlayerRegistrationGUI(client, errorMessage = "") {
+
+}
+
+// ---------------------------------------------------------------------------
+
+function showPlayerNewCharacterGUI(client) {
+
+}
+
+// ---------------------------------------------------------------------------
+
+function showPlayerPromptGUI(client, promptMessage, answerYesText, answerNoText) {
+
+}
+
+// ---------------------------------------------------------------------------
+
+function showPlayerPromptGUI(client) {
+
+}
+
+// ---------------------------------------------------------------------------

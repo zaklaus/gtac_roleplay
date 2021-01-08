79686f15f7be03800fee93b9e24c6deb9e3dbdfe
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-01-08 00:17:11 -0600

    Add log level const

--- scripts/shared/const.js
+++ scripts/shared/const.js
@@ -13,3 +13,10 @@ const AG_LABEL_JOB = 1;
 const AG_LABEL_BUSINESS = 2;
 const AG_LABEL_HOUSE = 3;
 const AG_LABEL_EXIT = 3;
+
+// Log Levels
+const LOG_NONE = 0;
+const LOG_INFO = 1;
+const LOG_WARN = 2;
+const LOG_ERROR = 4;
+const LOG_VERBOSE = 8;

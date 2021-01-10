479ea4b2f67b6d0ef5a00a1771681f4d2b038ab6
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-01-09 22:40:34 -0600

    Add tutorial stuff (unfinished)

---
+++ scripts/server/tutorial.js
@@ -0,0 +1,57 @@
+// ===========================================================================
+// Asshat-Gaming Roleplay
+// https://github.com/VortrexFTW/gtac_asshat_rp
+// Copyright (c) 2020 Asshat-Gaming (https://asshatgaming.com)
+// ---------------------------------------------------------------------------
+// FILE: tutorial.js
+// DESC: Provides tutorial functions and features
+// TYPE: Server (JavaScript)
+// ===========================================================================
+
+let tutorialSpawn = [
+    false,
+    [new Vec3(0.0, 0.0, 0.0), 0.0, 0, 0],
+    [new Vec3(0.0, 0.0, 0.0), 0.0, 0, 0],
+    [new Vec3(0.0, 0.0, 0.0), 0.0, 0, 0],
+    [new Vec3(0.0, 0.0, 0.0), 0.0],
+    [new Vec3(0.0, 0.0, 0.0), 0.0, 0, 0],
+];
+
+let tutorialVehicle = [
+    false,
+    [111, new Vec3(0.0, 0.0, 0.0)],
+    [175, new Vec3(0.0, 0.0, 0.0), 0.0],
+    [445, new Vec3(0.0, 0.0, 0.0), 0.0],
+    [445, new Vec3(0.0, 0.0, 0.0), 0.0],
+    [0, new Vec3(0.0, 0.0, 0.0), 0.0],
+];
+
+let tutorialItem = [
+    false,
+    [new Vec3(0.0, 0.0, 0.0)],
+    [new Vec3(0.0, 0.0, 0.0), 0.0],
+    [new Vec3(0.0, 0.0, 0.0), 0.0],
+    [new Vec3(0.0, 0.0, 0.0), 0.0],
+    [new Vec3(0.0, 0.0, 0.0), 0.0],
+];
+
+// ---------------------------------------------------------------------------
+
+function startTutorial(client) {
+    getPlayerData(client).tutorialItem = createGroundItem(tutorialItem[0], tutorialItem[1], tutorialItem[3]);
+    getPlayerData(client).tutorialVehicle = createGroundItem(tutorialItem[0], tutorialItem[1], tutorialItem[3]);
+}
+
+// ---------------------------------------------------------------------------
+
+function hasPlayerFinishedTutorial(client) {
+
+}
+
+// ---------------------------------------------------------------------------
+
+function isPlayerInTutorial(client) {
+
+}
+
+// ---------------------------------------------------------------------------

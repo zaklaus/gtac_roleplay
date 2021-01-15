9603faed15b70d5a77c46f259708334e97fab4d7
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-01-15 03:44:09 -0600

    Add handcuff item type script

---
+++ scripts/server/item/handcuff.js
@@ -0,0 +1,31 @@
+// ===========================================================================
+// Asshat-Gaming Roleplay
+// https://github.com/VortrexFTW/gtac_asshat_rp
+// Copyright (c) 2021 Asshat-Gaming (https://asshatgaming.com)
+// ---------------------------------------------------------------------------
+// FILE: handcuff.js
+// DESC: Provides features and usage for the handcuff item type
+// TYPE: Server (JavaScript)
+// ===========================================================================
+
+// ---------------------------------------------------------------------------
+
+function isPlayerHandCuffed(client) {
+    return (getPlayerData(client).pedState == AG_PEDSTATE_BINDED);
+}
+
+// ---------------------------------------------------------------------------
+
+function handCuffPlayer(client) {
+    getPlayerData(client).pedState = AG_PEDSTATE_BINDED;
+    setPlayerControlState(client, false);
+}
+
+// ---------------------------------------------------------------------------
+
+function unHandCuffPlayer(client) {
+    getPlayerData(client).pedState = AG_PEDSTATE_READY;
+    setPlayerControlState(client, true);
+}
+
+// ---------------------------------------------------------------------------

9da00e472f62c112e0f46619d054ee228331b591
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-03-04 04:45:04 -0600

    Add shared keystate and game consts

--- scripts/shared/const.js
+++ scripts/shared/const.js
@@ -31,4 +31,15 @@ const AG_WEAPON_DAMAGE_EVENT_EXTINGUISH = 3;
 const AG_WEAPON_DAMAGE_EVENT_MACE = 4;
 
 // Games
+const AG_GAME_GTA_III = 1;
+const AG_GAME_GTA_VC = 2;
+const AG_GAME_GTA_SA = 3;
+const AG_GAME_GTA_IV = 4;
 const AG_GAME_GTA_V = 50;
+
+// Key States
+const AG_KEYSTATE_NONE = 0;
+const AG_KEYSTATE_UP = 1;
+const AG_KEYSTATE_DOWN = 2;
+const AG_KEYSTATE_HOLDSHORT = 3;
+const AG_KEYSTATE_HOLDLONG = 4;

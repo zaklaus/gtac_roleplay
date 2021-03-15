f6ed9c88af407bb6a0fd5d53083150adbcb11726
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-03-15 12:30:35 -0500

    Update copyright year from 2020 to 2021

--- scripts/client/mouse-camera.js
+++ scripts/client/mouse-camera.js
@@ -2,7 +2,7 @@
 // ===========================================================================
 // Asshat-Gaming Roleplay
 // https://github.com/VortrexFTW/gtac_asshat_rp
-// Copyright (c) 2020 Asshat-Gaming (https://asshatgaming.com)
+// Copyright (c) 2021 Asshat-Gaming (https://asshatgaming.com)
 // ---------------------------------------------------------------------------
 // FILE: mouse-camera.js
 // DESC: Provides a freelook camera similar to SA for III and VC

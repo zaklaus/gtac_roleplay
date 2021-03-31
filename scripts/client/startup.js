0189304517b14272568fff9286c2d3620b019c81
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-03-30 21:44:34 -0500

    Add CS startup script

---
+++ scripts/client/startup.js
@@ -0,0 +1,30 @@
+// ===========================================================================
+// Asshat-Gaming Roleplay
+// https://github.com/VortrexFTW/gtac_asshat_rp
+// Copyright (c) 2021 Asshat-Gaming (https://asshatgaming.com)
+// ===========================================================================
+// FILE: startup.js
+// DESC: Provides startup/shutdown procedures
+// TYPE: Client (JavaScript)
+// ===========================================================================
+
+function initClientScripts() {
+    initGUIScript();
+    initNameTagScript();
+    initScoreBoardScript();
+    initMouseCameraScript();
+    initMessagingScript();
+    initServerScript();
+    initLogoScript();
+    init3DLabelScript();
+    initChatBoxScript();
+    initAFKScript();
+    initKeyBindScript();
+    initEventScript();
+}
+
+// ===========================================================================
+
+initClientScripts();
+
+// ===========================================================================

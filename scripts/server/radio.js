901f150efd61acc2a8aaf6c5c017bff719b85637
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-04-20 21:34:04 -0500

    Radio stream and volume funcs

--- scripts/server/radio.js
+++ scripts/server/radio.js
@@ -44,3 +44,74 @@ function loadRadioStationsFromDatabase() {
 }
 
 // ===========================================================================
+
+function playStreamingRadioCommand(command, params, client) {
+	if(areParamsEmpty(params)) {
+		messagePlayerSyntax(client, getCommandSyntaxText(command));
+		return false;
+	}
+
+	let radioStationId = params;
+
+	if(radioStationId != 0 && typeof radioStations[radioStationId-1] == "undefined") {
+		messagePlayerError(client, "That radio station ID does not exist!");
+		return false;
+	}
+
+	if(!isPlayerInAnyVehicle(client)) {
+
+	}
+
+	if(!getVehicleData(getPlayerVehicle(client))) {
+		messagePlayerError(client, "This is a random traffic vehicle and commands can't be used for it.");
+		return false;
+	}
+
+	if(radioStationId == 0) {
+		getVehicleData(getPlayerVehicle(client)).streamingRadioStation = -1;
+		getPlayerData(client).streamingRadioStation = -1;
+		meActionToNearbyPlayers(client, `turns off their vehicle's radio`);
+
+		let clients = getClients();
+		for(let i in clients) {
+			if(getPlayerVehicle(client) == getPlayerVehicle(clients[i])) {
+				playRadioStreamForPlayer(clients[i], "");
+			}
+		}
+		return false;
+	}
+
+	getVehicleData(getPlayerVehicle(client)).streamingRadioStation = radioStationId-1;
+	getPlayerData(client).streamingRadioStation = radioStationId-1;
+	meActionToNearbyPlayers(client, `changes their vehicle's radio station to ${radioStations[radioStationId-1].name}`);
+
+	let clients = getClients();
+	for(let i in clients) {
+		if(getPlayerVehicle(client) == getPlayerVehicle(clients[i])) {
+			playRadioStreamForPlayer(clients[i], radioStations[radioStationId-1].url);
+			setPlayerStreamingRadioVolume(client, getPlayerData(client).streamingRadioVolume);
+		}
+	}
+}
+
+// ===========================================================================
+
+function setStreamingRadioVolumeCommand(command, params, client) {
+	if(areParamsEmpty(params)) {
+		messagePlayerSyntax(client, getCommandSyntaxText(command));
+		return false;
+	}
+
+	let volumeLevel = params;
+
+	if(isNaN(volumeLevel)) {
+		messagePlayerError(client, "Volume level must a number");
+		return false;
+	}
+
+	setPlayerStreamingRadioVolume(client, toInteger(volumeLevel)/100);
+	getPlayerData(client).streamingRadioVolume = toInteger(volumeLevel)/100;
+	messagePlayerSuccess(client, `You set your streaming radio volume to ${volumeLevel}`);
+}
+
+// ===========================================================================

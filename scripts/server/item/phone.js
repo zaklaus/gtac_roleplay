3f56a0127b9a569abb2ebad5c4695784df2be636
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-04-23 18:20:40 -0500

    Start adding phone item features

--- scripts/server/item/phone.js
+++ scripts/server/item/phone.js
@@ -9,3 +9,36 @@
 // ===========================================================================
 
 // ===========================================================================
+
+function getItemWithPhoneNumber(phoneNumber) {
+    for(let i in getServerData().items) {
+        if(getItemTypeData(getItemData(i).itemTypeIndex).useType == AG_ITEM_USETYPE_PHONE) {
+            if(getItemData(i).value == phoneNumber) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+// ===========================================================================
+
+function isPhoneItemEnabled(itemIndex) {
+    return getItemData(itemIndex).enabled;
+}
+
+// ===========================================================================
+
+function ringPhoneForNearbyPlayers(itemIndex) {
+    if(isPhoneItemEnabled(itemIndex)) {
+
+    }
+}
+
+// ===========================================================================
+
+function phoneTransmit(radioFrequency, messageText, transmittingPlayer) {
+    phoneOutgoingToNearbyPlayers(transmittingPlayer, messageText);
+}
+
+// ===========================================================================

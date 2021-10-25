9c051c0d706c0bd6a5cd5d3d2ec0e3148594fbcf
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-10-25 13:08:51 -0500

    Some new unfinished stuff

---
+++ scripts/server/npc/taxi.js
@@ -0,0 +1,28 @@
+// ===========================================================================
+// Vortrex's Roleplay Resource
+// https://github.com/VortrexFTW/gtac_roleplay
+// ===========================================================================
+// FILE: taxi.js
+// DESC: Provides taxi driver NPC interaction and functionality
+// TYPE: Server (JavaScript)
+// ===========================================================================
+
+function attemptToSignalToNearbyTaxi(client) {
+    if(!isPlayerLoggedIn(client)) {
+        return false;
+    }
+
+    if(!isPlayerSpawned(client)) {
+        return false;
+    }
+
+    let nearbyTaxis = getVehicles().filter((v) > getPlayerPosition(client).distance(v.position) <= 15 && isTaxiVehicle(v));
+
+    let closestTaxi = nearbyTaxis.reduce((i, j) => (i.position.distance(pos) < j.position.distance(pos)) ? i : j);
+    if(!closestTaxi.getOccupant(0).isType(ELEMENT_PLAYER)) {
+        setVehicleCruiseSpeed(closestTaxi, 0.0);
+        setVehicleLockedState(closestTaxi, false);
+    }
+}
+
+// ===========================================================================
---
+++ scripts/server/theme.js
@@ -0,0 +1,9 @@
+// ===========================================================================
+// Vortrex's Roleplay Resource
+// https://github.com/VortrexFTW/gtac_roleplay
+// ===========================================================================
+// FILE: theme.js
+// DESC: Provides server theme content
+// TYPE: Server (JavaScript)
+// ===========================================================================
+

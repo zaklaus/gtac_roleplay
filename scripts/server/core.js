a892ed778b8f12e58f2c260d53c49071866ff38d
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-11-24 07:22:00 -0600

    Change default server log level

--- scripts/server/core.js
+++ scripts/server/core.js
@@ -9,7 +9,7 @@
 
 let scriptVersion = "1.0";
 let serverStartTime = 0;
-let logLevel = LOG_INFO;
+let logLevel = LOG_ERROR|LOG_WARN|LOG_INFO;
 
 // ===========================================================================
 

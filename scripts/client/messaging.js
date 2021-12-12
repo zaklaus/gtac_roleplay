42b1d6e70a4afcafc3bc6a5266f14fb2519c6e57
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-12-11 20:12:21 -0600

    Move small game message up slightly

--- scripts/client/messaging.js
+++ scripts/client/messaging.js
@@ -52,7 +52,7 @@ function processSmallGameMessageRendering() {
     if(renderSmallGameMessage) {
         if(smallGameMessageFont != null) {
             if(smallGameMessageFont != "") {
-                smallGameMessageFont.render(smallGameMessageText, [0, game.height-50], game.width, 0.5, 0.0, smallGameMessageFont.size, smallGameMessageColour, true, true, false, true);
+                smallGameMessageFont.render(smallGameMessageText, [0, game.height-90], game.width, 0.5, 0.0, smallGameMessageFont.size, smallGameMessageColour, true, true, false, true);
             }
         }
     }

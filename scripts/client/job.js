a55ae11e672002072f33c68c2f9594c2cdd3caed
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-01-17 07:39:57 -0600

    Add line of sight check for nametags

--- scripts/client/job.js
+++ scripts/client/job.js
@@ -82,5 +82,3 @@ function blinkJobRouteStopBlip(times, position, colour) {
         jobRouteStopBlip = game.createBlip(position, 0, 2, colour);
     }, 500*times+1);
 }
-
-// ===========================================================================

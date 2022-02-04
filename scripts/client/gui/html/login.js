80453b7b5c15a8ce701b79c702843e2be3130962
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-02-04 10:35:34 -0600

    Work on HTML login GUI

---
+++ scripts/client/gui/html/login.js
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Connected RP: Login</title>
+        <style type="text/css" rel="stylesheet">
+            .input-box
+            {
+                font-family: "Roboto";
+                font-size: 14px;
+                border-style: solid;
+                border-colour: #0066AA;
+                border-radius: 2px;
+                color: #0066AA;
+            };
+        </style>
+    </head>
+    <body>
+    </body>
+</html>

6b463e1566a00dba1f99be4c0c548ec37124b1b2
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-02-20 18:03:30 -0600

    Add utils for content resources

---
+++ scripts/client/content.js
@@ -0,0 +1,65 @@
+// ===========================================================================
+// Vortrex's Roleplay Resource
+// https://github.com/VortrexFTW/gtac_roleplay
+// ===========================================================================
+// FILE: content.js
+// DESC: Provides connection to extra content resources
+// TYPE: Client (JavaScript)
+// ===========================================================================
+
+function getCustomImage(imageName) {
+    let contentResource = findResourceByName(getGameData().extraContentResource[getGame()]);
+    if(contentResource != null) {
+        if(contentResource.isStarted) {
+            let image = contentResource.exports.getCustomImage(imageName);
+            if(image != null) {
+                return image;
+            }
+        }
+    }
+    return false;
+}
+
+// ===========================================================================
+
+function getCustomFont(fontName) {
+    let contentResource = findResourceByName(getGameData().extraContentResource[getGame()]);
+    if(contentResource != null) {
+        if(contentResource.isStarted) {
+            let font = contentResource.exports.getCustomFont(fontName);
+            if(font != null) {
+                return font;
+            }
+        }
+    }
+    return false;
+}
+
+// ===========================================================================
+
+function getCustomAudio(audioName) {
+    let contentResource = findResourceByName(getGameData().extraContentResource[getGame()]);
+    if(contentResource != null) {
+        if(contentResource.isStarted) {
+            let audioFile = contentResource.exports.getCustomAudio(audioName);
+            if(audioFile != null) {
+                return audioFile;
+            }
+        }
+    }
+    return false;
+}
+
+// ===========================================================================
+
+function playCustomAudio(audioName, volume = 0.5, loop = false) {
+    let contentResource = findResourceByName(getGameData().extraContentResource[getGame()]);
+    if(contentResource != null) {
+        if(contentResource.isStarted) {
+            contentResource.exports.playCustomAudio(audioName, volume, loop);
+        }
+    }
+    return false;
+}
+
+// ===========================================================================

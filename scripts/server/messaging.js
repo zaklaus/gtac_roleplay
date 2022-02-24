7daa6927704988c2cb6a660d9a5021c0d24ba4fc
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-02-24 15:45:30 -0600

    Offload colour replace to client

--- scripts/server/messaging.js
+++ scripts/server/messaging.js
@@ -33,19 +33,19 @@ function messageAdminAction(messageText) {
  *
  */
 function messagePlayerNormal(client, messageText, colour = COLOUR_WHITE) {
-    if(isConsole(client) || client == null) {
-        logToConsole(LOG_INFO, `${removeColoursInMessage(messageText)}`);
-    }
+    //if(isConsole(client) || client == null) {
+    //    logToConsole(LOG_INFO, `${removeColoursInMessage(messageText)}`);
+    //}
 
-    sendChatBoxMessageToPlayer(client, `${replaceColoursInMessage(messageText)}`, colour);
+    sendChatBoxMessageToPlayer(client, messageText, colour);
     return true;
 }
 
 // ===========================================================================
 
 function messageAdmins(messageText, colour = getColourByName("softRed")) {
-    let plainMessage = removeColoursInMessage(messageText);
-    console.warn(`🛡️ ${plainMessage}`);
+    //let plainMessage = removeColoursInMessage(messageText);
+    //console.warn(`🛡️ ${plainMessage}`);
 
     let clients = getClients();
     for(let i in clients) {
@@ -54,9 +54,9 @@ function messageAdmins(messageText, colour = getColourByName("softRed")) {
         }
     }
 
-    if(getServerConfig().discordConfig.sendAdminEvents) {
-        messageDiscordAdminChannel(plainMessage);
-    }
+    //if(getServerConfig().discordConfig.sendAdminEvents) {
+    //    messageDiscordAdminChannel(plainMessage);
+    //}
 }
 
 // ===========================================================================
@@ -232,17 +232,6 @@ function clearChatBox(client) {
 	for(let i = 0; i <= 20; i++) {
 		messageClient(" ", client, COLOUR_WHITE);
 	}
-}
-
-// ===========================================================================
-
-function replaceEmojiInString(messageString) {
-	for(let i in emojiReplaceString) {
-		while(messageString.indexOf(emojiReplaceString[i][0]) != -1) {
-			messageString = messageString.replace(emojiReplaceString[i][0], emojiReplaceString[i][1]);
-		}
-	}
-	return messageString;
 }
 
 // ===========================================================================

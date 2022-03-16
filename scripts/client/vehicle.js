5eb8ac4ea6e467e83f8de118f454939569aaf97a
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-03-16 17:14:02 -0500

    Add clientside vehicle script (unfinished)

--- scripts/client/vehicle.js
+++ scripts/client/vehicle.js
@@ -6,3 +6,13 @@
 // DESC: Provides vehicle functions and arrays with data
 // TYPE: Client (JavaScript)
 // ===========================================================================
+
+function receiveVehicleFromServer(vehicleId, position, model, colour1, colour2, colour3 = 0, colour4 = 0) {
+	logToConsole(LOG_DEBUG, `[VRR.Job] Received vehicle ${vehicleId} (${getVehicleNameFromModel(model, getGame())}) from server`);
+
+	if(getGame() == VRR_GAME_GTA_IV) {
+
+	}
+}
+
+// ===========================================================================

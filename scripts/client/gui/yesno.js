f24351af98e2553cdee5498b0accee211eb5a766
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-03-19 00:01:15 -0500

    Fix button sizes in prompt GUI

--- scripts/client/gui/yesno.js
+++ scripts/client/gui/yesno.js
@@ -48,7 +48,7 @@ function initYesNoDialogGUI() {
         },
     });
 
-    yesNoDialog.yesButton = yesNoDialog.window.button(5, 105, 190, 30, 'YES', {
+    yesNoDialog.yesButton = yesNoDialog.window.button(5, 105, 193, 30, 'YES', {
         main: {
             backgroundColour: toColour(primaryColour[0], primaryColour[1], primaryColour[2], buttonAlpha),
             textColour: toColour(primaryTextColour[0], primaryTextColour[1], primaryTextColour[2], 255),
@@ -61,7 +61,7 @@ function initYesNoDialogGUI() {
         },
     }, yesNoDialogAnswerYes);
 
-    yesNoDialog.noButton = yesNoDialog.window.button(200, 105, 190, 30, 'NO', {
+    yesNoDialog.noButton = yesNoDialog.window.button(203, 105, 192, 30, 'NO', {
         main: {
             backgroundColour: toColour(primaryColour[0], primaryColour[1], primaryColour[2], buttonAlpha),
             textColour: toColour(primaryTextColour[0], primaryTextColour[1], primaryTextColour[2], 255),
